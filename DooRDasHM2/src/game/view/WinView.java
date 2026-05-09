c0a459b7fbff5b074497730af04e8a6b47282eb1
Hemo06 <hemoadel06@gmail.com>
2026-05-09 15:06:05 +0300

    Added winnerRole in winview class

--- DooRDasHM2/src/game/view/WinView.java
+++ DooRDasHM2/src/game/view/WinView.java
@@ -6,24 +6,25 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class WinView extends VBox {
-	
-	public WinView(String winnerName, int finalEnergy) {
+
+	public WinView(String winnerName, String winnerRole, int finalEnergy) {
 		this.setSpacing(30);
 		this.setAlignment(Pos.CENTER);
 		this.setStyle("-fx-background-color: #27ae60;");
-		
+
 		Label winlab = new Label("VICTORY!");
 		winlab.setStyle("-fx-font-size: 48px; -fx-text-fill: white; -fx-font-weight: bold;");
-		
-		Label details = new Label(winnerName + " won with " + finalEnergy + " energy!");
+
+		Label details = new Label(winnerName + " (" + winnerRole + ") won with " + finalEnergy + " energy!");
 		details.setStyle("-fx-font-size: 20px; -fx-text-fill: white;");
-		
+
 		Button restartBtn = new Button("Return to Main Menu");
 		restartBtn.setPrefSize(200, 50);
 		restartBtn.setOnAction(e -> ViewManager.updateView(new StartView()));
+
+		this.getChildren().addAll(winlab, details, restartBtn);
+
 	}
-	
-    
-	
+
 
 }
